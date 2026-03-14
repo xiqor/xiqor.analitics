389aa6130043f129e99f38aad994d67d68292d5e
Max <xiqor@list.ru>
2026-03-14 19:52:40 +0300

    added error handling for connection.py

--- db/connection.py
+++ db/connection.py
@@ -5,10 +5,14 @@ import psycopg2
 load_dotenv() # later move it to main or app.py
 
 def get_connection():
-    return psycopg2.connect(
-        dbname = os.getenv('DB_NAME'),
-        user = os.getenv('DB_USER'),
-        password = os.getenv('DB_PASSWORD'),
-        host = os.getenv('DB_HOST'),
-        port= int(os.getenv('DB_PORT'))
-    )
+    try:
+        connection = psycopg2.connect(
+            dbname = os.getenv('DB_NAME'),
+            user = os.getenv('DB_USER'),
+            password = os.getenv('DB_PASSWORD'),
+            host = os.getenv('DB_HOST'),
+            port= int(os.getenv('DB_PORT'))
+        )
+        return connection
+    except Exception as e:
+        print(f"An error occurred: {e}")
